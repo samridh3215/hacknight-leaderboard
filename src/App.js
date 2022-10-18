c1a4e0918d5d549a5449c8f5165548c2042da4e8
Saksham-13 <sakshamalok13@gmail.com>
2022-10-18 21:58:33 +0530

    second genesis

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import logo from './hn_logo.png'
+import Scores from './logtable'
+
+function App() {
+  return(
+      <>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet"/>
+        <div className="App">
+        <img alt="hacknight logo" src={logo}/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css"/>
+          <h1>ACM PESUECC Hacknight 3.0</h1>
+          <h1>Leaderboard</h1>
+          <br/>
+          <Scores />
+        </div>
+      </>
+  );
+}
+
+export default App;
